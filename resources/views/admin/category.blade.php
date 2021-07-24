14f9e8eb3b610e0590a55d90be53435d1aae44aa
devboyarif <arif14arif15@gmail.com>
2021-07-24 23:27:03 +0600

    category livewire create

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.admin')
 
+@section('breadcrumbs')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0">Category</h1>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                <li class="breadcrumb-item active">Category</li>
+            </ol>
+        </div>
+    </div>
+@endsection
+
 @section('content')
-    asdas
+    @livewire('category.category')
 @endsection
 
 
